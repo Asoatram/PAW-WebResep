d83f1c75c2989c96b7cd69596cdd32bd6892bc2f
slwnyladis <salwanaylaadistri@mail.ugm.ac.id>
2024-11-26 12:53:25 +0700

    feat: added upload page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,44 +1,47 @@
-'use client'
+'use client';
 
 import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/SideBar";
-import "./layout.modules.css"
-import {AuthProvider, useAuth} from "@/context/AuthContext";
+import "./layout.modules.css";
+import { AuthProvider, useAuth } from "@/context/AuthContext";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
+
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
 });
 
-
 export default function RootLayout({
-                                     children,
-                                   }: Readonly<{ children: React.ReactNode }>) {
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
   return (
+    <>
+      {/* Fragment digunakan untuk membungkus elemen JSX */}
       <html lang="en">
-      <body
+        <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-      <AuthProvider>
-        {/* The entire layout is wrapped by AuthProvider */}
-        <div className="layout">
-          <Navbar />
-          <div className="body">
-            <AuthAwareSidebar />
-            <div className="content">{children}</div>
-          </div>
-        </div>
-      </AuthProvider>
-      </body>
+        >
+          <AuthProvider>
+            {/* The entire layout is wrapped by AuthProvider */}
+            <div className="layout">
+              <Navbar />
+              <div className="body">
+                <AuthAwareSidebar />
+                <div className="content">{children}</div>
+              </div>
+            </div>
+          </AuthProvider>
+        </body>
       </html>
+    </>
   );
 }
 
