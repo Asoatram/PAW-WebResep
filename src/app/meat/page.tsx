4c14c9b7173057218a8a6e5961be908554a5d9e3
slwnyladis <salwanaylaadistri@mail.ugm.ac.id>
2024-11-26 16:48:44 +0700

    update carousel & layout

--- src/app/meat/page.tsx
+++ src/app/meat/page.tsx
@@ -1,21 +1,20 @@
-// src/app/meat/page.tsx
-'use client'
+'use client';
 
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import FoodCard from "@/components/Card";
 
 export default function MeatPage() {
     const [recipes, setRecipes] = useState([]);
 
     useEffect(() => {
-        // Fetch vegan recipes from your API
+        // Fetch meat recipes from your API
         const fetchMeatRecipes = async () => {
             try {
                 const response = await fetch('/api/recipes?tag=Meat'); // Replace with your API endpoint
                 const data = await response.json();
                 setRecipes(data);
             } catch (error) {
-                console.error('Error fetching vegan recipes:', error);
+                console.error('Error fetching meat recipes:', error);
             }
         };
 
@@ -24,8 +23,8 @@ export default function MeatPage() {
 
     return (
         <div>
-            <h1>Meat Recipes</h1>
-            <hr className={"bg-black"}/>
+            <h1 className="text-3xl font-bold text-gray-800 mb-4">Meat Recipes</h1>
+            <hr className="bg-black" />
             <div className="grid grid-cols-4 gap-4 m-2">
                 {recipes.map((recipe, index) => (
                     <FoodCard
@@ -41,5 +40,4 @@ export default function MeatPage() {
             </div>
         </div>
     );
-  }
-  
+}
